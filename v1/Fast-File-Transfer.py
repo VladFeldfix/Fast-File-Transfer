61fdb252b8b8c636cc900e5a2d2c4ec3ab206826
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-06-27 09:35:00 +0300

    Add files via upload

--- v1/Fast-File-Transfer.py
+++ v1/Fast-File-Transfer.py
@@ -8,7 +8,7 @@ class main:
         self.sc = SmartConsole("Fast File Transfer", "1.0")
         
         # set-up main memu
-        self.sc.main_menu["RUN"] = self.run
+        self.sc.add_main_menu_item("RUN", self.run)
 
         # get settings
         self.path_from = self.sc.get_setting("From")
